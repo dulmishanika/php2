e509839ae9a4cd43ac6ad195a3705f5cf90613ed
dulmishanika <56398705+dulmishanika@users.noreply.github.com>
2019-10-13 23:34:12 +0530

    Add files via upload

---
+++ php2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Case Sensitivity in PHP</title>
+</head>
+<body>
+
+<?php
+
+$color = "yellow";
+$Color = "red";
+$COLOR = "green";
+ 
+
+echo "The color of the sky is " . $color . "<br>";
+echo "The color of the sun is " . $Color . "<br>";
+echo "The color of the tree is " . $COLOR . "<br>";
+?>
+
+</body>
+</html>                                		
